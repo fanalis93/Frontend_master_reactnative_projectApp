c909f02246526d430a4df8aeeabf010a599594e0
fanalis93 <fayek.lucse44@gmail.com>
2021-04-26 23:01:07 +0800

    init

--- App.js
+++ App.js
@@ -1,21 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import React from "react";
+import { View, Text, SafeAreaView, StyleSheet } from "react-native";
+import { StatusBar } from "expo-status-bar";
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <StatusBar style="dark" />
+      <View style={[styles.pink, styles.container]}>
+        <Text style={styles.text}>
+          Here are some boxes of different colors!
+        </Text>
+        <View style={[styles.cyanBox, styles.box]}>
+          <Text style={styles.boxText}>Cyan: #2aa198</Text>
+        </View>
+        <View style={[styles.blueBox, styles.box]}>
+          <Text style={styles.boxText}>Blue: #268bd2</Text>
+        </View>
+        <View style={[styles.magentaBox, styles.box]}>
+          <Text style={styles.boxText}>Cyan: #d33682</Text>
+        </View>
+        <View style={[styles.orangeBox, styles.box]}>
+          <Text style={styles.boxText}>Orange: #cb4b16</Text>
+        </View>
+      </View>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
+    marginVertical: 35,
+    //backgroundColor: "yellow",
+    paddingVertical: 10,
+    paddingHorizontal: 30,
+    borderColor: "pink",
+    borderWidth: 5,
+    //alignItems: "center",
+    //justifyContent: "center",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  pink: {
+    //backgroundColor: "pink",
+  },
+
+  text: {
+    fontSize: 15,
+    fontWeight: "bold",
+  },
+  safeArea: {
+    flex: 2,
+  },
+  box: {
+    padding: 8,
+    marginTop: 5,
+    borderRadius: 8,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  cyanBox: {
+    backgroundColor: "#2aa198",
+  },
+  blueBox: {
+    backgroundColor: "#268bd2",
+  },
+  magentaBox: {
+    backgroundColor: "#d33682",
+  },
+  orangeBox: {
+    backgroundColor: "#cb4b16",
+  },
+  boxText: {
+    color: "white",
+    fontWeight: "bold",
   },
 });
+
+export default App;
